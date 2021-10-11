cb7cd3a9c7d4ffe37c1fd86df77ebe0656d315d0
Shaper0k <Novomoskovsk08082002@gmail.com>
2021-10-11 17:46:27 +0300

    Commit v5

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -1,61 +1,67 @@
 package web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import web.model.Role;
 import web.model.User;
+import web.service.RoleService;
 import web.service.UserService;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 
-@RequestMapping("/admin")
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
+
 
     private final UserService userService;
 
+    @Autowired
     public AdminController(UserService userService) {
         this.userService = userService;
     }
 
 
     @GetMapping
-    public String listUsers(Model model) {
-        model.addAttribute("allUsers", userService.getAllUsers());
-        return "adminpage";
+    public String allUsers(ModelMap model) {
+        model.addAttribute("users", userService.getAllUsers());
+        return "admin-panel";
     }
 
-    @GetMapping(value = "/new")
-    public String newUser(Model model) {
-        model.addAttribute("user", new User());
-        return "new";
-    }
-
-    @PostMapping(value = "admin/add-user")
-    public String addUser(User user) {
-        userService.addUser(user);
-        return "redirect:/admin";
-    }
-
-    @GetMapping(value = "/edit/{id}")
-    public String editUserForm(@PathVariable("id") long id, Model model) {
-        User user = userService.getUser(id);
-        model.addAttribute("user", user);
+    @GetMapping("/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
         return "edit";
     }
 
     @PostMapping(value = "/edit")
-    public String editUser(User user) {
+    public String editUser(@ModelAttribute("user") User user) {
         userService.updateUser(user);
         return "redirect:/admin";
     }
 
-    @GetMapping(value = "/remove/{id}")
-    public String removeUser(@PathVariable("id") long id) {
+    @GetMapping("/delete/{id}")
+    public String deleteUserById(@PathVariable("id") Long id) {
         userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+    @GetMapping(value = "/add")
+    public String addUser(Model model) {
+        model.addAttribute("user", new User());
+        return "add";
+    }
+
+    @PostMapping(value = "/add")
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
         return "redirect:/admin";
     }
 }
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,19 +1,13 @@
 package web.controller;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import web.model.User;
 import web.service.UserService;
 
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
-@RequestMapping("/user")
 public class UserController {
 
 	private final UserService userService;
@@ -22,9 +16,9 @@ public class UserController {
 		this.userService = userService;
 	}
 
-	@GetMapping
-	public String userInfo(@AuthenticationPrincipal User user, Model model) {
-		model.addAttribute("user", user);
-		return "userpage";
+	@GetMapping(value = "/user")
+	public String getUserPage2(Model model, Principal principal) {
+		model.addAttribute("users", userService.findUserByUsername(principal.getName()));
+		return "user-panel";
 	}
 }
